8f6232ede676587ac3bd09d97131bae6e82af97b
Abobului-Ioan <75037928+Abobului-Ioan@users.noreply.github.com>
2021-06-03 13:56:14 +0300

    Am adăugat documentația specifică proiectului.

--- InterfataUtilizatorForms/Form2.Designer.cs
+++ InterfataUtilizatorForms/Form2.Designer.cs
@@ -44,7 +44,7 @@ namespace InterfataUtilizatorForms
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(150, 32);
             this.button1.TabIndex = 9;
-            this.button1.Text = "Click pentru a te intoarce";
+            this.button1.Text = "Înapoi la meniul principal";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
